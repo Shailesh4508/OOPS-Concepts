b56f5451d9c3b0112afc35e43b7bfbc6a750d419
Shailesh4508 <shailesh4508singh@gmail.com>
2025-02-24 17:40:38 +0530

    done

--- src/ex_18_OOPs_Constructors/Lab_155_Cars2_Constructor.java
+++ src/ex_18_OOPs_Constructors/Lab_155_Cars2_Constructor.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Lab_155_Cars2_Constructor {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Model Name for Mahindra");
-        String model_name = sc.next();
         car2 Mahindra = new car2("Scorpio",2004);
         System.out.println(Mahindra.model);
         System.out.println(Mahindra.year);
