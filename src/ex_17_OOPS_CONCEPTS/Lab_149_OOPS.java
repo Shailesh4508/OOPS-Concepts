f6f1e77c5d86a88de2069edfa74f1aba0d30598d
Shailesh4508 <shailesh4508singh@gmail.com>
2025-02-24 15:42:44 +0530

    done

---
+++ src/ex_17_OOPS_CONCEPTS/Lab_149_OOPS.java
@@ -0,0 +1,18 @@
+package ex_17_OOPS_CONCEPTS;
+
+public class Lab_149_OOPS {
+    // Attributes
+    String name;
+    int age;
+    String gender;
+    int salary;
+    long phone_no;
+    String address;
+
+
+
+    // Behaviour
+    void speak(){}
+    void eat(){}
+    void sleep(){};
+}
---
+++ src/ex_17_OOPS_CONCEPTS/Lab_150_Dog.java
@@ -0,0 +1,14 @@
+package ex_17_OOPS_CONCEPTS;
+
+public class Lab_150_Dog {
+    String name;
+    String breed;
+    String color_eyes;
+
+
+    void walk() {
+    }
+
+    void bark() {
+    }
+}
